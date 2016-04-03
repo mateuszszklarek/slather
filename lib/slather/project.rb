1b0856d4de603d6ec581fbf5483a8c9009298c7f
Mateusz Szklarek <mateusz.szklarek@elpassion.pl>
2016-04-03 10:37:24 +0200

    Fixed issue with directory for Coverage.profdata in Xcode 7.3

--- lib/slather/project.rb
+++ lib/slather/project.rb
@@ -129,18 +129,8 @@ module Slather
 
     def profdata_coverage_dir
       raise StandardError, "The specified build directory (#{self.build_directory}) does not exist" unless File.exists?(self.build_directory)
-      dir = nil
-      if self.scheme
-        dir = Dir[File.join("#{build_directory}","/**/CodeCoverage/#{self.scheme}")].first
-      else
-        dir = Dir[File.join("#{build_directory}","/**/#{first_product_name}")].first
-      end
-
-      if dir == nil
-        # Xcode 7.3 moved the location of Coverage.profdata
-        dir = Dir[File.join("#{build_directory}","/**/CodeCoverage")].first
-      end
-
+      # Xcode 7.3 moved the location of Coverage.profdata
+      dir = Dir[File.join("#{build_directory}","/**/CodeCoverage")].first
       raise StandardError, "No coverage directory found. Are you sure your project is setup for generating coverage files? Try `slather setup your/project.xcodeproj`" unless dir != nil
       dir
     end
